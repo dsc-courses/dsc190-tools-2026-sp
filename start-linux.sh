2ee5a1f4aaa75ff38dd5a95c31b5edbf13c453be
Justin Eldridge <eldridgejm@gmail.com>
2026-03-29 17:05:00 -0700

    Added hostname.

--- start-linux.sh
+++ start-linux.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-docker run -it --rm -v dsc190-tools-starter-home:/home/student ghcr.io/dsc-courses/dsc190-tools-starter
+docker run -it --rm \
+    -v dsc190-tools-starter-home:/home/student \
+    -h dsc190-tools-starter \
+    ghcr.io/dsc-courses/dsc190-tools-starter
