7186f03d4aa251ddf197c262cae0a9dd2e45fd3f
Justin Eldridge <eldridgejm@gmail.com>
2026-03-31 14:01:24 -0700

    Updated the start-linux.sh script to pull latest image.

--- start-linux.sh
+++ start-linux.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+docker pull ghcr.io/dsc-courses/dsc190-tools-starter:latest
 docker run -it --rm \
     -v dsc190-tools-starter-home:/home/student \
     -h dsc190-tools-starter \
