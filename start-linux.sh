0989a6deee39a89175375f602e225302f0e1ae65
Justin Eldridge <eldridgejm@gmail.com>
2026-04-09 18:40:01 -0700

    Added options to start-linux.sh script.

--- start-linux.sh
+++ start-linux.sh
@@ -3,4 +3,5 @@ docker pull ghcr.io/dsc-courses/dsc190-tools-starter:latest
 docker run -it --rm \
     -v dsc190-tools-starter-home:/home/student \
     -h dsc190-tools-starter \
+    $@ \
     ghcr.io/dsc-courses/dsc190-tools-starter:latest
